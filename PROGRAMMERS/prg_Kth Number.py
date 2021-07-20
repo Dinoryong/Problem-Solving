4d61226573b6927c29f91f33d2f096a6b092ef4d
dinoryong <biiregrey2@gmail.com>
2021-07-20 17:27:36 +0900

    Create : create kth number.py solution

--- PROGRAMMERS/prg_Kth Number.py
+++ PROGRAMMERS/prg_Kth Number.py
@@ -1,15 +1,37 @@
 """
 [프로그래머스 : K번째수]
+sol link :
 
 [topics]
-정렬
+Array
 
 [sketch]
 
 """
+# 틀린 이유 : 변수에 command[x] 를 할당해줘야한다
+# sort 할 때는 array.sort() 를 바로 해주면 된다.
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        command[0] = i
+        command[1] = j
+        command[2] = k
+        arr = array[i-1:j]
+        arr = arr.sort()
+        answer.append(arr[k-1])
+    return answer
+
 #
-
-
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i = command[0]
+        j = command[1]
+        k = command[2]
+        arr = array[i-1:j]
+        arr.sort()
+        answer.append(arr[k-1])
+    return answer
 
 
 
